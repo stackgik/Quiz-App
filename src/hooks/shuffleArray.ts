b6ef1029226be8081a9b5fd31c422fbdd0552da2
stackgik <stackgik@gmail.com>
2024-02-23 07:50:53 +0100

    convert the shuffleArray javascript file to a typescript file and also transform the algorithm to use typecsript

--- src/hooks/shuffleArray.ts
+++ src/hooks/shuffleArray.ts
@@ -1,8 +1,10 @@
 // Fisher-Yates (or Knuth) shuffle algorithm
-export function shuffleArray(array) {
+function shuffleArray<T>(array: T[]) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
   }
   return array;
 }
+
+export default shuffleArray;
